46a66232949ed0728c4cfeca4c245d53da6c3ff8
AHN HO JUNG <labeldock@me.com>
2018-06-19 13:40:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![CircleCI](https://circleci.com/gh/temping/vue-simple/tree/master.svg?style=shield)](https://circleci.com/gh/temping/vue-simple/tree/master)
+
+# next-edge-template
+
+## Installation
+This is a project template for [vue-cli](https://github.com/vuejs/vue-cli)(vue-cli 2.x<=).
+
+``` bash
+vue init temping/next-edge-template myprojectname
+```
---
+++ meta.js
@@ -0,0 +1,34 @@
+module.exports = {
+  helpers: {
+    raw: function(options) {
+      return options.fn(this)
+    }
+  },
+  prompts: {
+    name: {
+      type: "string",
+      required: true,
+      message: "App name"
+    },
+    description: {
+      type: "string",
+      required: false,
+      message: "App description",
+      default: "Nuxt(edge) Project"
+    },
+    author: {
+      type: "string",
+      required: false,
+      message: "Author"
+    }
+  },
+  completeMessage:[
+    "{{#inPlace}}",
+    "To get started:\n\n  npm install \n  npm run dev",
+    "\n\nshortcut:\n\n  npm i && npm run start",
+    "{{else}}",
+    "To get started:\n\n  cd {{destDirName}}\n  npm install \n  npm run start",
+    "\n\nshortcut:\n\n  cd {{destDirName}} && npm i && npm run start",
+    "{{/inPlace}}"
+  ].join("")
+}
